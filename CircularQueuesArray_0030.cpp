ac67fda83da19664eeb09064649358124fa38c81
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-05-31 14:40:54 +0700

    cek jika antrian hanya memiliki sat elemen dengan if

--- CircularQueuesArray_0030.cpp
+++ CircularQueuesArray_0030.cpp
@@ -46,3 +46,8 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+		// cek jika antrian hanya memiliki satu element
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
