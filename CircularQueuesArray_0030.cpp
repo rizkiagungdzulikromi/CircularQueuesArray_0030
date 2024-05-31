77c5cfad614842bdc18ba2dc8bb4e685b9ba7906
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-05-31 14:45:16 +0700

    iterasi dari awal array hingga rear

--- CircularQueuesArray_0030.cpp
+++ CircularQueuesArray_0030.cpp
@@ -87,3 +87,14 @@ public:
 				FRONT_position++;
 			}
 
+			FRONT_position = 0;
+
+			//interasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
