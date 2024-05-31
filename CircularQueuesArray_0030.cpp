ee381bfe95321c5f20de7917fa29475815ab1fff
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-05-31 14:46:18 +0700

    FINAL COMMIT

--- CircularQueuesArray_0030.cpp
+++ CircularQueuesArray_0030.cpp
@@ -103,3 +103,41 @@ int main() {
 	char ch;
 
 
+	while (true) {
+		try {
+			cout << "menu" << endl;
+			cout << "1. Iplement insert oprator" << endl;
+			cout << "2. Iplement delete optration" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1_4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+	return 0;
+}
